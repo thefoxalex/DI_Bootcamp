5c541d899006dd6b836df05f2640cac334a916a0
thefoxalex <alex.ogden511@gmail.com>
2025-12-24 20:07:50 +0200

    Updating Week 1, Day 6 exercises and Week 2 Mini Project

--- Week1/Day6/ExercisesXP/exercises.py
+++ Week1/Day6/ExercisesXP/exercises.py
@@ -75,3 +75,50 @@ for age in family.values():
 # Bonus:
 
 # Allow the user to input family members’ names and ages, then calculate the total ticket cost.
+
+# Exercise 3: Zara
+# Key Python Topics:
+
+# Creating dictionaries
+# Accessing and modifying dictionary elements
+# Dictionary methods like .pop() and .update()
+
+
+# Instructions
+# Create and manipulate a dictionary that contains information about the Zara brand.
+
+brand = {
+    "name": "Zara"
+    creation_date: 1975
+    creator_name: "Amancio Ortega Gaona"
+    type_of_clothes: "men", "women", "children", "home"
+    international_competitors: "Gap", "H&M", "Benetton"
+    number_stores: 7000
+    major_color: 
+        France: blue, 
+        Spain: red, 
+        US: pink, green
+}
+
+# Brand Information:
+
+
+
+
+# Create a dictionary called brand with the provided data.
+# Modify and access the dictionary as follows:
+# Change the value of number_stores to 2.
+# Print a sentence describing Zara’s clients using the type_of_clothes key.
+# Add a new key country_creation with the value Spain.
+# Check if international_competitors exists and, if so, add “Desigual” to the list.
+# Delete the creation_date key.
+# Print the last item in international_competitors.
+# Print the major colors in the US.
+# Print the number of keys in the dictionary.
+# Print all keys of the dictionary.
+
+
+# Bonus:
+
+# Create another dictionary called more_on_zara with creation_date and number_stores. Merge this dictionary with the original brand dictionary and print the result.
+
--- Week2/Day3/Exercises/mini_project.py
+++ Week2/Day3/Exercises/mini_project.py
@@ -1,88 +1,20 @@
-tic_tac_toe_board = [['x ','x',' x',]]
-print(f'___{tic_tac_toe_board[0][0]}|___{tic_tac_toe_board[0][1]}|___{tic_tac_toe_board[0],[2]}')
-print(f'___{tic_tac_toe_board[0][0]}|___{tic_tac_toe_board[0][1]}|___{tic_tac_toe_board[0],[2]}')
-print(f'___{tic_tac_toe_board[0][0]}|___{tic_tac_toe_board[0][1]}|___{tic_tac_toe_board[0],[2]}')
+board = {
+    '7': ' ', '8': ' ', '9': ' ',
+    '4': ' ', '5': ' ', '6': ' ',
+    '1': ' ', '2': ' ', '3': ' ',
+}
 
 
 
-# The game board is represented by a dictionary
-theBoard = {'7': ' ' , '8': ' ' , '9': ' ' ,
-            '4': ' ' , '5': ' ' , '6': ' ' ,
-            '1': ' ' , '2': ' ' , '3': ' ' }
+def display_board(board):
+    print('***************')
+    print('*  ' + board['7'] + ' | ' + board['8'] + ' | ' + board['9'] + '  *')
+    print('* ---|---|--- *')
+    print('*  ' + board['4'] + ' | ' + board['5'] + ' | ' + board['6'] + '  *')
+    print('* ---|---|--- *')
+    print('*  ' + board['1'] + ' | ' + board['2'] + ' | ' + board['3'] + '  *')
+    print('***************')
+display_board(board)
 
-def printBoard(board):
-    """
-    Prints the current state of the tic-tac-toe board.
-    The keys in the dictionary are arranged to match a 3x3 grid,
-    like a keyboard's number pad.
-    """
-    print(board['7'] + '|' + board['8'] + '|' + board['9'])
-    print('-+-+-')
-    print(board['4'] + '|' + board['5'] + '|' + board['6'])
-    print('-+-+-')
-    print(board['1'] + '|' + board['2'] + '|' + board['3'])
-
-def checkWin(board, player):
-    """
-    Checks if the current player has won the game by covering all 3
-    spaces in any row, column, or diagonal.
-    """
-    # Check horizontal wins
-    if board['7'] == board['8'] == board['9'] == player:
-        return True
-    if board['4'] == board['5'] == board['6'] == player:
-        return True
-    if board['1'] == board['2'] == board['3'] == player:
-        return True
-    # Check vertical wins
-    if board['7'] == board['4'] == board['1'] == player:
-        return True
-    if board['8'] == board['5'] == board['2'] == player:
-        return True
-    if board['9'] == board['6'] == board['3'] == player:
-        return True
-    # Check diagonal wins
-    if board['7'] == board['5'] == board['3'] == player:
-        return True
-    if board['9'] == board['5'] == board['1'] == player:
-        return True
-    return False
-
-def game():
-    """
-    The main game loop that handles turns, input, and game status checks.
-    """
-    turn = 'X'
-    count = 0
-    while count < 9:
-        printBoard(theBoard)
-        print("It's your turn, " + turn + ". Move to which place?")
-        move = input()
-        
-        if move in theBoard and theBoard[move] == ' ':
-            theBoard[move] = turn
-            count += 1
-        else:
-            print("That place is already filled or invalid input. Try again!")
-            continue
-
-        if count >= 5: # A win is only possible after at least 5 moves
-            if checkWin(theBoard, turn):
-                printBoard(theBoard)
-                print("\nGame Over.")
-                print("**** " + turn + " won! ****")
-                return
-
-        # Switch turns
-        if turn == 'X':
-            turn = 'O'
-        else:
-            turn = 'X'
-
-    # If the loop finishes without a winner, it's a tie
-    printBoard(theBoard)
-    print("\nGame Over.")
-    print("It's a Tie!!")
-
-if __name__ == "__main__":
-    game()
+def player_input(player):
+    user1_position = input("Please enter a position on the board using the numbers 1-9 (e.g., for the upper right square, you would type in 9, like the layout of a number pad): ")
