4fdb77a14b43d42dcea011adad87ba6f748c35c7
thefoxalex <alex.ogden511@gmail.com>
2026-02-19 07:53:43 +0200

    Week 5, Day 1: Daily challenge

---
+++ Week5/DailyChallenge/dailychallenge.py
@@ -0,0 +1,24 @@
+class Farm:
+    def __init__(self, farm_name):
+        self.farm_name = farm_name
+        self.animals = {}
+
+    def add_animal(self, animal_type, count=1):
+        if animal_type in self.animals:
+            self.animals[animal_type] += count
+        else:
+            self.animals[animal_type] = count
+
+    def get_info(self):
+        info_string = f"{self.farm_name}'s farm\n\n"
+        for animal, count in self.animals.items():
+            info_string += f"{animal} : {count}\n"
+        info_string += "\n    E-I-E-I-0!"
+        return info_string
+
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow', 5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+print(macdonald.get_info())
